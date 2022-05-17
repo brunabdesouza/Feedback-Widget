11c55fc2316b61b5ed1723d435dd55525f4c42f8
brunabdesouza <bruna.bdesouza@gmail.com>
2022-05-17 12:09:12 +1000

    map to get each key and value from object and display each item

--- src/components/WidgetForm.tsx
+++ src/components/WidgetForm.tsx
@@ -1,18 +1,42 @@
 import { CloseButton } from "./CloseButton";
 
-export function WidgetForm() {
+import bugImageUrl from '../assets/bug.svg'
+import ideaImageUrl from '../assets/idea.svg'
+import thoughtImageUrl from '../assets/thought.svg'
 
-  const feedbackTypes = {
-    BUG: {
-      title: 'Issue'
-    },
-    IDEA: {
-      title: 'Idea'
-    },
-    OTHER: {
-      title: 'Other'
+const feedbackTypes = {
+  BUG: {
+    title: 'Issue',
+    image: {
+      source: bugImageUrl,
+      alt: 'Worm image'
+    }
+  },
+  IDEA: {
+    title: 'Idea',
+    image: {
+      source: ideaImageUrl,
+      alt: 'Lamp image'
+    }
+  },
+  OTHER: {
+    title: 'Other',
+    image: {
+      source: thoughtImageUrl,
+      alt: 'Thinking cloud image'
     }
   }
+}
+
+// Object.entries(feedbackTypes) returns => 
+// [ 
+//  ['BUG', {...}]
+//  ['IDEA', {...}]
+//  ['THOUGHT', {...}]
+// ]
+
+export function WidgetForm() {
+
 
   return (
     <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
@@ -22,7 +46,21 @@ export function WidgetForm() {
         <CloseButton />
       </header>
 
-      <div className="flex py-8 gap-2 w-full"></div>
+      <div className="flex py-8 gap-2 w-full">
+        { Object.entries(feedbackTypes).map(([key, value]) => {
+          return (
+            <button 
+              key={key}
+              className="bg-zinc-800 rounded-lg py-5 w-24 flex-1 flex flex-col items-center gap-2 border-2 border-transparent hover:border-brand-500 focus:border-brand-500 focus:outline-none"
+              onClick={}
+              type="button"
+            >
+              <img src={value.image.source} alt={value.image.alt} />
+              <span>{value.title}</span>
+            </button>
+          )
+        })}
+      </div>
 
       <footer className="text-xs text-neutral-400">
         Made with ♥ by <a className="underline underline-offset-2" href="https://github.com/brunabdesouza/Feedback-Widget">Bruna</a>
