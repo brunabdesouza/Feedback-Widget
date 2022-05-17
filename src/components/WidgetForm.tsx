d6bb2824b693b94416c0d940031520582f70a8b9
brunabdesouza <bruna.bdesouza@gmail.com>
2022-05-17 11:43:49 +1000

    object to store feedback types and added svg icons

--- src/components/WidgetForm.tsx
+++ src/components/WidgetForm.tsx
@@ -1,6 +1,19 @@
 import { CloseButton } from "./CloseButton";
 
 export function WidgetForm() {
+
+  const feedbackTypes = {
+    BUG: {
+      title: 'Issue'
+    },
+    IDEA: {
+      title: 'Idea'
+    },
+    OTHER: {
+      title: 'Other'
+    }
+  }
+
   return (
     <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
       <header>
@@ -8,6 +21,8 @@ export function WidgetForm() {
 
         <CloseButton />
       </header>
+
+      <div className="flex py-8 gap-2 w-full"></div>
 
       <footer className="text-xs text-neutral-400">
         Made with ♥ by <a className="underline underline-offset-2" href="https://github.com/brunabdesouza/Feedback-Widget">Bruna</a>
