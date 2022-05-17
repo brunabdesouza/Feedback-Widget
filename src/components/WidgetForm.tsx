3cc69e402256ba858907ac73a0d1d0e76ebf0473
brunabdesouza <bruna.bdesouza@gmail.com>
2022-05-17 11:15:58 +1000

    Component for CloseButton and styling

--- src/components/WidgetForm.tsx
+++ src/components/WidgetForm.tsx
@@ -1,7 +1,17 @@
+import { CloseButton } from "./CloseButton";
+
 export function WidgetForm() {
   return (
     <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
-      Hello World 
+      <header>
+        <span className="text-xl leading-6">Give your feedback</span>
+
+        <CloseButton />
+      </header>
+
+      <footer className="text-xs text-neutral-400">
+        Made with ♥ by <a className="underline underline-offset-2" href="https://github.com/brunabdesouza/Feedback-Widget">Bruna</a>
+      </footer>
     </div>
   );
 }
